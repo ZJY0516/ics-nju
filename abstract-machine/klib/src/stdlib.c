b5e04af1adf4acf3e95490503485b20e9f089dce
tracer-ics2023 <tracer@njuics.org>
2023-10-20 23:19:48 +0800

    >  compile NEMU 211830010 朱江云 Linux zjy-pc 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:19:48 up 2 days, 23:24,  4 users,  load average: 0.86, 0.75, 0.79

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -5,41 +5,48 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
 
-int rand(void) {
-  // RAND_MAX assumed to be 32767
-  next = next * 1103515245 + 12345;
-  return (unsigned int)(next/65536) % 32768;
+int rand(void)
+{
+    // RAND_MAX assumed to be 32767
+    next = next * 1103515245 + 12345;
+    return (unsigned int)(next / 65536) % 32768;
 }
 
-void srand(unsigned int seed) {
-  next = seed;
-}
+void srand(unsigned int seed) { next = seed; }
 
-int abs(int x) {
-  return (x < 0 ? -x : x);
-}
+int abs(int x) { return (x < 0 ? -x : x); }
 
-int atoi(const char* nptr) {
-  int x = 0;
-  while (*nptr == ' ') { nptr ++; }
-  while (*nptr >= '0' && *nptr <= '9') {
-    x = x * 10 + *nptr - '0';
-    nptr ++;
-  }
-  return x;
+int atoi(const char *nptr)
+{
+    int x = 0;
+    while (*nptr == ' ') {
+        nptr++;
+    }
+    while (*nptr >= '0' && *nptr <= '9') {
+        x = x * 10 + *nptr - '0';
+        nptr++;
+    }
+    return x;
 }
-
-void *malloc(size_t size) {
-  // On native, malloc() will be called during initializaion of C runtime.
-  // Therefore do not call panic() here, else it will yield a dead recursion:
-  //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
+static char *addr = NULL;
+void *malloc(size_t size)
+{
+    // On native, malloc() will be called during initializaion of C runtime.
+    // Therefore do not call panic() here, else it will yield a dead recursion:
+    //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+    // panic("Not implemented");
+    if (addr == NULL)
+        addr = heap.start;
+    size_t alignment = sizeof(size_t); // memory alignment
+    size_t aligned_size = (size + alignment - 1) / alignment * alignment;
+    char *ret = addr;
+    addr += aligned_size;
+    return ret;
 #endif
-  return NULL;
+    return NULL;
 }
 
-void free(void *ptr) {
-}
+void free(void *ptr) {}
 
 #endif
