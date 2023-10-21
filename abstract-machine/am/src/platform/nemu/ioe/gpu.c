d281a29811ef69d370839a098cb701cb5d5c0339
tracer-ics2023 <tracer@njuics.org>
2023-10-21 12:06:59 +0800

    >  compile NEMU 211830010 朱江云 Linux zjy-pc 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:06:59 up 3 days, 12:12,  4 users,  load average: 1.06, 1.08, 1.00

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,23 +3,48 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
-void __am_gpu_init() {
+void __am_gpu_init() {}
+
+void __am_gpu_config(AM_GPU_CONFIG_T *cfg)
+{
+    // nemu/src/device/vga.c
+    // vgactl_port_base[0] = (screen_width() << 16) | screen_height();
+    uint32_t screen_info = inl(VGACTL_ADDR);
+    uint32_t Width = screen_info >> 16;
+    uint32_t Height = screen_info & 0xFFFF;
+    *cfg = (AM_GPU_CONFIG_T){
+        .present = true,
+        .has_accel = false,
+        .width = Width,
+        .height = Height,
+        .vmemsz = Height * Width * sizeof(uint32_t) // vmemsz=?
+    };
 }
 
-void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
-  };
+void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl)
+{
+    uint32_t screen_info = inl(VGACTL_ADDR);
+    uint32_t Width = screen_info >> 16;
+    uint32_t Height = screen_info & 0xFFFF;
+    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+    // if (!ctl->sync) {
+    //     return;
+    // }
+    int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+    // if (w == 0 || h == 0)
+    //     return;
+    uint32_t *pixels = (uint32_t *)ctl->pixels;
+    int len = (x + w >= Width) ? Width - x : w;
+    for (int j = 0; j < h; j++) {
+        if (y + j < Height) {
+            for (int i = 0; i < len; i++) {
+                fb[(j + y) * Width + x + i] = pixels[j * w + i];
+            }
+        }
+    }
+    if (ctl->sync) {
+        outl(SYNC_ADDR, 1);
+    }
 }
 
-void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
-  }
-}
-
-void __am_gpu_status(AM_GPU_STATUS_T *status) {
-  status->ready = true;
-}
+void __am_gpu_status(AM_GPU_STATUS_T *status) { status->ready = true; }
