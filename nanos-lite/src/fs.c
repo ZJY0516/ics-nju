b28fb731221ac39eb2446493628a09a182d865b6
tracer-ics2023 <tracer@njuics.org>
2023-12-14 19:11:21 +0800

    >  compile NEMU 211830010 朱江云 Linux zjy-pc 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:11:21 up 14 days,  6:12,  3 users,  load average: 0.98, 0.82, 0.76

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -69,7 +69,7 @@ size_t fs_write(int fd, const void *buf, size_t len)
 {
     size_t offset = file_table[fd].disk_offset + file_table[fd].open_offset;
     file_table[fd].open_offset += len;
-    assert(file_table[fd].open_offset < file_table[fd].size);
+    // assert(file_table[fd].open_offset < file_table[fd].size);
     return file_table[fd].write(buf, offset, len);
 }
 
