278f5068b1fc24df4191de4b6908b7d59d0c6e6a
tracer-ics2023 <tracer@njuics.org>
2023-12-14 14:55:21 +0800

    >  compile NEMU 211830010 朱江云 Linux zjy-pc 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:55:21 up 14 days,  1:56,  3 users,  load average: 0.78, 0.89, 1.02

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,9 +1,14 @@
 #include <common.h>
 #include "syscall.h"
+#include "../../nemu/include/generated/autoconf.h"
 void do_syscall(Context *c)
 {
     uintptr_t a[4];
     a[0] = c->GPR1;
+    uintptr_t call_para[3];
+    call_para[0] = c->GPR2;
+    call_para[1] = c->GPR3;
+    call_para[2] = c->GPR4;
 
     switch (a[0]) {
     case SYS_exit:
@@ -13,7 +18,19 @@ void do_syscall(Context *c)
         yield();
         c->GPRx = 0;
         break;
+    case SYS_write:
+        if (call_para[0] == 1) {
+            for (int i = 0; i < call_para[2]; i++) {
+                putch(*(char *)(call_para[1] + i));
+            }
+        }
+        c->GPRx = call_para[2];
+        break;
+
     default:
         panic("Unhandled syscall ID = %d", a[0]);
     }
+#ifdef CONFIG_STRACE
+    Log("syscall ID = %d  return value = %d", a[0], c->GPRx);
+#endif
 }
