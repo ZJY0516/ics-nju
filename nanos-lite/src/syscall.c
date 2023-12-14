bfc8d66ace9e9ab481c6ae8d4e212242f5f29d03
tracer-ics2023 <tracer@njuics.org>
2023-12-14 19:16:24 +0800

    >  compile NEMU 211830010 朱江云 Linux zjy-pc 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:16:24 up 14 days,  6:17,  3 users,  load average: 0.52, 0.62, 0.69

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -20,12 +20,13 @@ void do_syscall(Context *c)
         c->GPRx = 0;
         break;
     case SYS_write:
-        // if (call_para[0] == 1) {
-        //     for (int i = 0; i < call_para[2]; i++) {
-        //         putch(*(char *)(call_para[1] + i));
-        //     }
-        // }
-        // c->GPRx = call_para[2];
+        if (call_para[0] == 1) {
+            for (int i = 0; i < call_para[2]; i++) {
+                putch(*(char *)(call_para[1] + i));
+            }
+            c->GPRx = call_para[2];
+            break;
+        }
         c->GPRx =
             fs_write(call_para[0], (const void *)call_para[1], call_para[2]);
         break;
