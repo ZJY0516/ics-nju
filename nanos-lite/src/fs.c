1f3faf347c2737f31c086baadf4689bd87c15b2b
tracer-ics2023 <tracer@njuics.org>
2023-12-14 16:16:12 +0800

    >  compile NEMU 211830010 朱江云 Linux zjy-pc 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:16:12 up 14 days,  3:17,  3 users,  load average: 1.95, 1.49, 1.16

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,36 +1,57 @@
 #include <fs.h>
 
-typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
-typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
+typedef size_t (*ReadFn)(void *buf, size_t offset, size_t len);
+typedef size_t (*WriteFn)(const void *buf, size_t offset, size_t len);
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
+int fs_close(int fd);
 
 typedef struct {
-  char *name;
-  size_t size;
-  size_t disk_offset;
-  ReadFn read;
-  WriteFn write;
+    char *name;
+    size_t size;
+    size_t disk_offset;
+    ReadFn read;
+    WriteFn write;
+    size_t open_offset;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
+enum { FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB };
 
-size_t invalid_read(void *buf, size_t offset, size_t len) {
-  panic("should not reach here");
-  return 0;
+size_t invalid_read(void *buf, size_t offset, size_t len)
+{
+    panic("should not reach here");
+    return 0;
 }
 
-size_t invalid_write(const void *buf, size_t offset, size_t len) {
-  panic("should not reach here");
-  return 0;
+size_t invalid_write(const void *buf, size_t offset, size_t len)
+{
+    panic("should not reach here");
+    return 0;
 }
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+    [FD_STDIN] = {"stdin", 0, 0, invalid_read, invalid_write},
+    [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
+    [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
 
-void init_fs() {
-  // TODO: initialize the size of /dev/fb
+#define NR_FILE (int)sizeof(file_table) / sizeof(file_table[0])
+
+int fs_open(const char *pathname, int flags, int mode)
+{
+    for (int i = 0; i < NR_FILE; i++) {
+        if (strcmp(pathname, file_table[i].name) == 0) {
+            return i;
+        }
+    }
+    assert(0);
+}
+
+void init_fs()
+{
+    // TODO: initialize the size of /dev/fb
 }
