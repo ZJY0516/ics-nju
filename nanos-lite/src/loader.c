029b3f0c4ec549eca0d435390b631d6ccb9ecb7d
tracer-ics2023 <tracer@njuics.org>
2023-12-14 13:54:06 +0800

    >  compile NEMU 211830010 朱江云 Linux zjy-pc 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:54:06 up 14 days, 54 min,  3 users,  load average: 1.04, 0.92, 0.99

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,21 +2,40 @@
 #include <elf.h>
 
 #ifdef __LP64__
-# define Elf_Ehdr Elf64_Ehdr
-# define Elf_Phdr Elf64_Phdr
+#define Elf_Ehdr Elf64_Ehdr
+#define Elf_Phdr Elf64_Phdr
 #else
-# define Elf_Ehdr Elf32_Ehdr
-# define Elf_Phdr Elf32_Phdr
+#define Elf_Ehdr Elf32_Ehdr
+#define Elf_Phdr Elf32_Phdr
 #endif
 
-static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+static uintptr_t loader(PCB *pcb, const char *filename)
+{
+    Elf_Ehdr elf_header;
+    ramdisk_read(&elf_header, 0, sizeof(Elf_Ehdr));
+    assert((*(uint32_t *)elf_header.e_ident == 0x464c457f));
+    int numSegments = elf_header.e_phnum;
+    Elf_Phdr *programHeaders =
+        (Elf_Phdr *)malloc(sizeof(Elf_Phdr) * numSegments);
+    ramdisk_read(programHeaders, elf_header.e_phoff,
+                 sizeof(Elf_Phdr) * numSegments);
+    for (int i = 0; i < numSegments; i++) {
+        if (programHeaders[i].p_type == PT_LOAD) {
+            ramdisk_read((void *)programHeaders[i].p_vaddr,
+                         programHeaders[i].p_offset,
+                         programHeaders[i].p_filesz); // file size or mem size?
+            memset((void *)(programHeaders[i].p_vaddr +
+                            programHeaders[i].p_filesz),
+                   0, programHeaders[i].p_memsz - programHeaders[i].p_filesz);
+        }
+    }
+    return elf_header.e_entry; // important!
 }
 
-void naive_uload(PCB *pcb, const char *filename) {
-  uintptr_t entry = loader(pcb, filename);
-  Log("Jump to entry = %p", entry);
-  ((void(*)())entry) ();
+void naive_uload(PCB *pcb, const char *filename)
+{
+    uintptr_t entry = loader(pcb, filename);
+    Log("Jump to entry = %p", entry);
+    ((void (*)())entry)();
 }
-
