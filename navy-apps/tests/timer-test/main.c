fe7736c7a3f879a95699d546587246bfa200b404
tracer-ics2023 <tracer@njuics.org>
2023-12-17 12:17:34 +0800

    >  compile NEMU 211830010 朱江云 Linux zjy-pc 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:17:34 up 16 days, 23:18,  3 users,  load average: 0.34, 0.33, 0.47

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 #include <sys/time.h>
+#include <NDL.h>
+
+#define NDL
 
 int main()
 {
+#ifdef NDL
+    uint32_t msec = 0;
+#elif
     struct timeval tv;
     __time_t sec = 0;
+#endif
     while (1) {
+#ifdef NDL
+        if (NDL_GetTicks() > msec + 500) {
+            printf("msec: %u\n", NDL_GetTicks());
+            msec = NDL_GetTicks();
+        }
+#elif
         gettimeofday(&tv, NULL);
         if (tv.tv_sec > sec) {
             printf("+1s  %d\n", sizeof(tv.tv_sec));
             printf("gettimeofday: %ld.%06ld\n", tv.tv_sec, tv.tv_usec);
             sec = tv.tv_sec;
         }
+#endif
     }
     return 0;
 }
