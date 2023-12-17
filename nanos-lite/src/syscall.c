5f428224905811ff2ac028234439b796acaadba1
tracer-ics2023 <tracer@njuics.org>
2023-12-17 10:48:10 +0800

    >  compile NEMU 211830010 朱江云 Linux zjy-pc 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:48:10 up 16 days, 21:48,  3 users,  load average: 1.11, 1.14, 0.94

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -51,8 +51,8 @@ void do_syscall(Context *c)
         struct timeval *tv = (struct timeval *)call_para[0];
         AM_TIMER_UPTIME_T uptime;
         ioe_read(AM_TIMER_UPTIME, &uptime);
-        tv->tv_usec = uptime.us % 1000000;
-        tv->tv_sec = uptime.us / 1000000;
+        tv->tv_usec = (int32_t)uptime.us % 1000000;
+        tv->tv_sec = (int32_t)uptime.us / 1000000;
         // printf("size of tv->sec %d\n", sizeof(tv->tv_sec));
         c->GPRx = 0;
         break;
--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -9,6 +9,7 @@ int main()
         gettimeofday(&tv, NULL);
         if (tv.tv_sec > sec) {
             printf("+1s  %d\n", sizeof(tv.tv_sec));
+            printf("gettimeofday: %ld.%06ld\n", tv.tv_sec, tv.tv_usec);
             sec = tv.tv_sec;
         }
     }
