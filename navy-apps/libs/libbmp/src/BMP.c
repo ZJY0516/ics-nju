9164ef1aeaaa65d082ca7478593cd317a5526eb4
tracer-ics2023 <tracer@njuics.org>
2023-12-18 16:27:53 +0800

    >  compile NEMU 211830010 朱江云 Linux zjy-pc 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:27:53 up 54 min,  3 users,  load average: 0.90, 0.80, 0.84

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -19,6 +19,9 @@ struct BitmapHeader {
     uint32_t clrused, clrimportant;
 } __attribute__((packed));
 
+int w = 128;
+int h = 128;
+uint32_t pixels[128 * 128];
 void *BMP_Load(const char *filename, int *width, int *height)
 {
     FILE *fp = fopen(filename, "r");
@@ -33,9 +36,9 @@ void *BMP_Load(const char *filename, int *width, int *height)
         return NULL;
     if (hdr.compression != 0)
         return NULL;
-    int w = hdr.width;
-    int h = hdr.height;
-    uint32_t *pixels = malloc(w * h * sizeof(uint32_t));
+    // int w = hdr.width;
+    // int h = hdr.height;
+    // uint32_t *pixels = malloc(w * h * sizeof(uint32_t));
     printf("222222222222222222222222\n");
     int line_off = (w * 3 + 3) & ~0x3;
     for (int i = 0; i < h; i++) {
