8d7605fafa4cdc3b789e4c068b68bb920b34cf39
tracer-ics2023 <tracer@njuics.org>
2023-12-18 13:26:32 +0800

    >  compile NEMU 211830010 朱江云 Linux zjy-pc 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:26:32 up 18 days, 27 min,  3 users,  load average: 1.55, 1.01, 0.97

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -42,6 +42,7 @@ void *BMP_Load(const char *filename, int *width, int *height)
         fseek(fp, hdr.offset + (h - 1 - i) * line_off, SEEK_SET);
         printf("44444444444444444444444444444444\n");
         int nread = fread(&pixels[w * i], 3, w, fp);
+        printf("55555555555555555555555\n");
         for (int j = w - 1; j >= 0; j--) {
             uint8_t b = *(((uint8_t *)&pixels[w * i]) + 3 * j);
             uint8_t g = *(((uint8_t *)&pixels[w * i]) + 3 * j + 1);
