4a01202ca414862f2621c0a7563b6ff987055c44
tracer-ics2023 <tracer@njuics.org>
2023-12-18 17:56:52 +0800

    >  compile NEMU 211830010 朱江云 Linux zjy-pc 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:56:52 up  2:23,  3 users,  load average: 0.86, 0.94, 0.90

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -39,11 +39,9 @@ void *BMP_Load(const char *filename, int *width, int *height)
     int w = hdr.width;
     int h = hdr.height;
     uint32_t *pixels = malloc(w * h * sizeof(uint32_t));
-    printf("222222222222222222222222\n");
     int line_off = (w * 3 + 3) & ~0x3;
     printf("44444444444444444444444444444444\n");
     printf("addr:%p\n", pixels);
-    printf("55555555555555555555555\n");
     for (int i = 0; i < h; i++) {
         fseek(fp, hdr.offset + (h - 1 - i) * line_off, SEEK_SET);
         int nread = fread(&pixels[w * i], 3, w, fp);
