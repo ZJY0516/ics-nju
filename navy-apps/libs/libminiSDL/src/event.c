7dc490c80e9bc679a98d32d3e273f32b56450d49
tracer-ics2023 <tracer@njuics.org>
2023-12-19 15:26:54 +0800

    >  compile NEMU 211830010 朱江云 Linux zjy-pc 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:26:54 up 23:53,  3 users,  load average: 1.14, 1.23, 1.04

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,29 +1,44 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <assert.h>
+#include <string.h>
 
 #define keyname(k) #k,
 
-static const char *keyname[] = {
-  "NONE",
-  _KEYS(keyname)
-};
+static const char *keyname[] = {"NONE", _KEYS(keyname)};
+#define NR_KEYS (sizeof(keyname) / sizeof(keyname[0]))
 
-int SDL_PushEvent(SDL_Event *ev) {
-  return 0;
+int SDL_PushEvent(SDL_Event *ev) { return 0; }
+
+int SDL_PollEvent(SDL_Event *ev) { return 0; }
+
+int SDL_WaitEvent(SDL_Event *event)
+{
+    // nanos-lite/src/device events_read()
+    char buf[32];
+    char key[16];
+    if (NDL_PollEvent(buf, sizeof(buf)) == 0)
+        return 0;
+    if (strncmp(buf, "kd", 2) == 0) {
+        sscanf(buf + 3, "%s\n", key);
+        event->type = SDL_KEYDOWN;
+    } else if (strncmp(buf, "ku", 2) == 0) {
+        sscanf(buf + 3, "%s\n", key);
+        event->type = SDL_KEYUP;
+    } else
+        assert(0);
+    for (int i = 0; i < NR_KEYS; i++) {
+        if (strcmp(key, keyname[i]) == 0) {
+            event->key.keysym.sym = i;
+            break;
+        }
+    }
+    return 1;
 }
 
-int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
+int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask)
+{
+    return 0;
 }
 
-int SDL_WaitEvent(SDL_Event *event) {
-  return 1;
-}
-
-int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
-  return 0;
-}
-
-uint8_t* SDL_GetKeyState(int *numkeys) {
-  return NULL;
-}
+uint8_t *SDL_GetKeyState(int *numkeys) { return NULL; }
