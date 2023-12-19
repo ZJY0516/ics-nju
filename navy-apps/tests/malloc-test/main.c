c2ac74e5e49c99f93330d6a8cbcebb6a4d89cca5
tracer-ics2023 <tracer@njuics.org>
2023-12-19 16:45:40 +0800

    >  compile NEMU 211830010 朱江云 Linux zjy-pc 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:45:40 up 1 day,  1:12,  3 users,  load average: 1.58, 1.13, 0.86

--- navy-apps/tests/malloc-test/main.c
+++ navy-apps/tests/malloc-test/main.c
@@ -12,5 +12,8 @@ int main()
     printf("addr:%p\n", p + 5);
     printf("addr:%p\n", p + 6);
     printf("addr:%p\n", p + 7);
+    uint32_t *q = malloc(128 * 128 * sizeof(uint32_t));
+    printf("addr:%p\n", q);
+    printf("addr:%p\n", q + 1);
     return 0;
 }
