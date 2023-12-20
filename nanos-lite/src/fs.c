e0b7c157cdf95d3b17ed559dc0428c51b5369afd
tracer-ics2023 <tracer@njuics.org>
2023-12-20 10:58:07 +0800

    >  compile NEMU 211830010 朱江云 Linux zjy-pc 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:58:07 up 1 day, 19:24,  3 users,  load average: 1.35, 1.95, 1.85

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -58,6 +58,7 @@ int fs_open(const char *pathname, int flags, int mode)
 {
     for (int i = 0; i < NR_FILE; i++) {
         if (strcmp(pathname, file_table[i].name) == 0) {
+            file_table[i].open_offset = 0;
             return i;
         }
     }
