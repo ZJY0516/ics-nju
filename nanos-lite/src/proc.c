4845432b2cefff5f836ce4ef1f12b25b0e1f538c
tracer-ics2023 <tracer@njuics.org>
2023-12-21 14:34:50 +0800

    >  compile NEMU 211830010 朱江云 Linux zjy-pc 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:34:50 up 2 days, 23:01,  3 users,  load average: 1.24, 1.03, 0.86

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,7 +27,7 @@ void init_proc()
     Log("Initializing processes...");
 
     // load program here
-    naive_uload(NULL, "/bin/menu"); // menu or menu? hhh
+    naive_uload(NULL, "/bin/hello"); // menu or menu? hhh
 }
 
 Context *schedule(Context *prev) { return NULL; }
