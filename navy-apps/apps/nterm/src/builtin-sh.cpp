c79b3df88b9c02b4a9119f87abf8cc2b52c57fa4
tracer-ics2023 <tracer@njuics.org>
2024-01-22 07:13:46 +0800

    >  compile NEMU 211830010 朱江云 Linux zjy-pc 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  07:13:46 up 5 days, 17:29,  4 users,  load average: 1.04, 0.76, 0.69

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -53,6 +53,7 @@ static void sh_handle_cmd(const char *cmd)
     char *p = strtok(path_cpy, ":");
     while (p != NULL) {
         setenv("PATH", p, 1);
+        printf("p: %s\n", p);
         if (execvp(argv[0], argv) >= 0) {
             setenv("PATH", path_cpy, 1);
             return;
