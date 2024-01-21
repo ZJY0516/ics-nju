849e5503e38220d34b0cad1e9352c71a2dec70ec
tracer-ics2023 <tracer@njuics.org>
2024-01-22 01:49:42 +0800

    >  compile NEMU 211830010 朱江云 Linux zjy-pc 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:49:42 up 5 days, 12:05,  4 users,  load average: 1.05, 1.04, 0.96

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -1,11 +1,17 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
   char *empty[] =  {NULL };
+  int argc = *(int *)args;
+  char **argv = (char **)(args + 1);
+  for (int i = 0; i < argc; i++) {
+      printf("libos argv: %s\n", argv[i]);
+  }
   environ = empty;
   exit(main(0, empty, empty));
   assert(0);
