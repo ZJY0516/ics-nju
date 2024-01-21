0189ea1c0c52e297bc34964eb7c300619202bf3c
tracer-ics2023 <tracer@njuics.org>
2024-01-22 00:29:15 +0800

    >  compile NEMU 211830010 朱江云 Linux zjy-pc 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:29:15 up 5 days, 10:45,  4 users,  load average: 1.29, 0.99, 0.74

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -34,7 +34,7 @@ void hello_fun(void *arg)
 
 void init_proc()
 {
-    char *pal_argv[] = {"--skip", "argv1", "argv2"};
+    char *pal_argv[] = {"--skip", "argv1", "argv2", NULL};
     context_kload(&pcb[0], hello_fun, "114");
     context_uload(&pcb[1], "/bin/pal", pal_argv, NULL);
     switch_boot_pcb();
