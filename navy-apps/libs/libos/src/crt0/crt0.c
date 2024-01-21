966f1a4ab5041dce80a0dda0fa477dad227fc78b
tracer-ics2023 <tracer@njuics.org>
2024-01-22 01:49:59 +0800

    >  compile NEMU 211830010 朱江云 Linux zjy-pc 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:49:59 up 5 days, 12:05,  4 users,  load average: 0.81, 0.98, 0.94

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -9,9 +9,6 @@ void call_main(uintptr_t *args) {
   char *empty[] =  {NULL };
   int argc = *(int *)args;
   char **argv = (char **)(args + 1);
-  for (int i = 0; i < argc; i++) {
-      printf("libos argv: %s\n", argv[i]);
-  }
   environ = empty;
   exit(main(0, empty, empty));
   assert(0);
